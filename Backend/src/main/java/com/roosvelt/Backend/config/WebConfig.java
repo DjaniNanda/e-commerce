664faa6907493c9995dbeec78aa9ec82dfb1ac87
DjaniNanda <djaninandapetrus@gmail.com>
2025-08-29 04:56:59 +0200

    Update WebConfig.java

--- Backend/src/main/java/com/roosvelt/Backend/config/WebConfig.java
+++ Backend/src/main/java/com/roosvelt/Backend/config/WebConfig.java
@@ -9,14 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("frontend.url")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .maxAge(3600);
-    }
 
     @Value("${app.upload.dir:../frontend/public/images/products}")
     private String uploadDir;
