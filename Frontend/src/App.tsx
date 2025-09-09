241dd07a4739c563a0729dc1eafb0db16df6d44e
DjaniNanda <djaninandapetrus@gmail.com>
2025-09-09 18:45:33 +0200

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import { TranslationProvider } from './context/TranslationContext';
+import './components styles/google-translate.css'; 
 import MainApp from './components/MainApp';
 import AdminPanel from './components/AdminPanel';
 import ProductDetails from './components/admin/ProductDetails';
@@ -16,14 +17,14 @@ function App() {
           <Routes>
             {/* Public Routes */}
             <Route path="/" element={<MainApp />} />
-            
+
             {/* Admin Routes */}
             <Route path="/admin" element={<AdminPanel />} />
             <Route path="/admin/products/add" element={<ProductForm />} />
             <Route path="/admin/products/edit/:id" element={<ProductForm />} />
             <Route path="/admin/products/:id" element={<ProductDetails />} />
             <Route path="/admin/orders/:id" element={<OrderDetails />} />
-            
+
             {/* Catch all - redirect to home */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
